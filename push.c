d9b7d4057ce7faf414649f238a8c8e347c25dc89
Matezzi777 <matezzi777@gmail.com>
2024-10-12 20:50:35 +0200

    IN PROGRESS

--- push.c
+++ push.c
@@ -2,10 +2,18 @@
 
 // void	pa(t_node *a, t_node *b)
 // {
-
+// 	ft_printf("pa\n");
+// 	if (!b)
+// 		return ;
+// 	else
+// 		push(a, b);
 // }
 
 // void	pb(t_node *a, t_node *b)
 // {
-	
+// 	ft_printf("pb\n");
+// 	if (!a)
+// 		return ;
+// 	else
+// 		push(b, a);
 // }
