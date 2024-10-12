814c1ab919d12ad1095485bbf45aa19dd7cf2b53
Matezzi777 <matezzi777@gmail.com>
2024-10-12 20:50:09 +0200

    ra(), rb() and rr() added

--- rotate.c
+++ rotate.c
@@ -1,11 +1,33 @@
 #include "push_swap.h"
 
-// void	rotate(t_node *stack)
-// {
+void	rotate(t_node *stack)
+{
+	int	new_last_value;
 
-// }
+	new_last_value = stack->value;
+	while (stack->next)
+	{
+		stack->value = stack->next->value;
+		stack = stack->next;
+	}
+	stack->value = new_last_value;
+}
 
-// void	rr(t_node *a, t_node *b)
-// {
-	
-// }
+void	ra(t_node *a)
+{
+	ft_printf("ra\n");
+	rotate(a);
+}
+
+void	rb(t_node *b)
+{
+	ft_printf("rb\n");
+	rotate(b);
+}
+
+void	rr(t_node *a, t_node *b)
+{
+	ft_printf("rr\n");
+	rotate(a);
+	rotate(b);
+}
