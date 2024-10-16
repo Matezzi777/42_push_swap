21e57561a8ad6d7f52b8eb9575eb11539ab39669
Matezzi777 <matezzi777@gmail.com>
2024-10-17 01:14:47 +0200

    ft_list_head() & ft_list_last() added

--- list_utils.c
+++ list_utils.c
@@ -38,3 +38,21 @@ int	ft_get_index_from_value(t_node *stack, int target)
 	}
 	return (-1);
 }
+
+t_node	*ft_list_head(t_node *stack)
+{
+	if (!(stack->prev))
+		return (stack);
+	while (stack->prev)
+		stack = stack->prev;
+	return (stack);
+}
+
+t_node	*ft_list_last(t_node *stack)
+{
+	if (!(stack->next))
+		return (stack);
+	while (stack->next)
+		stack = stack->next;
+	return (stack);
+}
